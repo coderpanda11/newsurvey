4b42201caa29cb078d7667bb1d8501d3b5f0dd3f
coderpanda11 <sidharthpandarpg1@gmail.com>
2024-12-11 13:35:48 +0530

    local

--- script.js
+++ script.js
@@ -220,7 +220,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         try {
             const data = await s3.getObject(params).promise();
             const surveyData = JSON.parse(data.Body.toString('utf-8'));
-            localStorage.setItem('surveyData', JSON.stringify(surveyData));
+            // localStorage.setItem('surveyData', JSON.stringify(surveyData));
             displaySurvey(surveyData);
         } catch (error) {
             console.error('Error fetching survey:', error);
